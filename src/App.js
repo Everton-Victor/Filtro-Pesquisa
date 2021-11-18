c789108bf2d3b8d2719db7ef310fe0cace226058
Everton-Victor <evertonvictor2019br@gmail.com>
2021-11-18 10:55:59 -0300

    Filtro de pesquisa
    Foi criado um filtro usando useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+const carros = [
+  { categoria: "Esporte", preco: "110000", modelo: "Golf GTI" },
+  { categoria: "Esporte", preco: "120000", modelo: "Camaro" },
+  { categoria: "SUV", preco: "85000", modelo: "HRV" },
+  { categoria: "SUV", preco: "83000", modelo: "T-Cross" },
+  { categoria: "Utilitário", preco: "125000", modelo: "Hillux" },
+  { categoria: "Utilitário", preco: "90000", modelo: "Ranger" },
+];
 
-function App() {
+const linhas = (categoria) => {
+  const linha = [];
+  carros.forEach((carro) => {
+    if (
+      carro.categoria.toUpperCase() === categoria.toUpperCase() ||
+      categoria === ""
+    ) {
+      linha.push(
+        <tr>
+          <td>{carro.categoria}</td>
+          <td>{carro.preco}</td>
+          <td>{carro.modelo}</td>
+        </tr>
+      );
+    }
+  });
+  return linha;
+};
+
+const TabelaCarros = (categoria) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table border="1" style={{ borderCollapse: "collapse" }}>
+      <thead>
+        <tr>
+          <th>Categoria</th>
+          <th>Preço</th>
+          <th>Modelo</th>
+        </tr>
+      </thead>
+      <tbody>{linhas(categoria)}</tbody>
+    </table>
+  );
+};
+
+const pesquisaCategoria = (categoria, setCategoria) => {
+  return (
+    <section>
+      <label>
+        Digite uma categoria
+        <input
+          type="text"
+          value={categoria}
+          onChange={(element) => {
+            setCategoria(element.target.value);
+          }}
+        />
+      </label>
+    </section>
+  );
+};
+export default function App() {
+  const [categoria, setCategoria] = useState("");
+
+  return (
+    <>
+      {pesquisaCategoria(categoria, setCategoria)}
+      {TabelaCarros(categoria)}
+    </>
   );
 }
-
-export default App;
